d4c0a850a6c99e18c2ae9f295bea232142d440cc
Mulia Nasution <mul14@users.noreply.github.com>
2023-11-03 20:12:35 +0700

    --wip-- [skip ci]

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import core from '@actions/core';
 import github from '@actions/github';
 import axios from 'axios';
+import { Octokit } from "@octokit/rest";
 
 try {
   (async () => {
-    const response = await axios.get('https://httpbin.org/get');
-    core.setOutput("data", response.data);
-    console.log(response.data);
+    // const response = await axios.get('https://httpbin.org/get');
+    // core.setOutput("data", response.data);
+    // console.log(response.data);
+
+    const response = await octokit.rest.pulls.checkIfMerged({
+      owner: 'mul14',
+      repo: 'demo',
+      pull_number: 3,
+    });
+
+    console.log(response);
+
   })();
 } catch (error) {
   core.setFailed(error.message);
